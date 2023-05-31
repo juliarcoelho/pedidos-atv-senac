6255037626e3addc92c508e2de12ef2f2dd2bc2e
juliarcoelho <juliarochx@gmail.com>
2023-05-31 16:11:11 -0300

    adicionando arquivos no git

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace prova
+{
+    class ItemPedido
+    {
+        public String descricao;
+        public double valorUnitario, quantidade;
+        public ItemPedido(double q, double v)
+        {
+            valorUnitario = v;
+            quantidade = q;
+        }
+    }
+    class Pedido
+    {
+        public List<ItemPedido> itemPedido;
+        public Pedido()
+        {
+            itemPedido = new List<ItemPedido>();
+        }
+        public void AdicionaPedido(ItemPedido item)
+        {
+            itemPedido.Add(item);
+        }
+        public double TotalizaPedido()
+        {
+            double total = 0;
+            foreach (var item in itemPedido)
+            {
+                
+                total = total + item.valorUnitario * item.quantidade;
+            }
+            
+            return total;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String descricao;
+            double valorUnitario, quantidade;
+            char fim = 's';
+
+            Pedido novoPedido = new Pedido();
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Olá");
+               
+                Console.WriteLine("Digite a descrição: ");
+                descricao = Console.ReadLine();
+                Console.WriteLine("Digite o valor unitário: ");
+                valorUnitario = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Digite a quantidade: ");
+                quantidade = Convert.ToDouble(Console.ReadLine());
+
+                ItemPedido novoItemPedido = new ItemPedido(quantidade, valorUnitario);
+                novoPedido.AdicionaPedido(novoItemPedido);
+
+                Console.WriteLine("Digite s caso queira adicionar mais pedidos ");
+                fim = Convert.ToChar(Console.ReadLine());
+
+            } while(fim == 's');
+
+            Console.WriteLine("Total: " + novoPedido.TotalizaPedido());
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Estudo da Utilização de Classe em C#: Cálculo de Valor Total do Pedido
+
+Este repositório contém um estudo da utilização de classes em C#, desenvolvido como parte do meu curso técnico no Senac. O projeto solicita dados, como descrição, valor e quantidade, permitindo adicionar mais de um item. Com esses dados, é calculado o valor total do pedido. O código utiliza estruturas de repetição, como `while` e `foreach`.
+
+## Pré-requisitos
+
+- Ambiente de desenvolvimento compatível com C# compatível
+- .NET Framework (ou versão superior)
